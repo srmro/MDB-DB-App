df60132185fa75123e5f95b963f2da15d1ebc026
srmro <833stephanie@gmail.com>
2025-02-03 23:41:08 -0700

    created to check if connected to the mongodb

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import mongoose from "mongoose";
 
 
 const app = express();
-const data = mongoose;
 
 app.use(express.json());
 
 
 const PORT = 3020;
+
+mongoose.connect('mongodb+srv://romero:loca4321@cualquier.ns93v.mongodb.net/Cualquier-Cosa?retryWrites=true&w=majority&appName=Cualquier')
+try {
+    console.log('Connected to Cualquier Cosa');
+} catch(error){
+console.error(error);
+}
 
 app.listen(PORT,() => {
     console.log(`Port is running on ${PORT}...`);
