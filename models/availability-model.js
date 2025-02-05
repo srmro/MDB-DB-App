30b1ba6ef35dbfaeff3d969adb2cedde09a470b2
srmro <833stephanie@gmail.com>
2025-02-04 19:08:31 -0700

    creating th the models/schemas

---
+++ models/availability-model.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const availabilitySchema = new mongoose.Schema ({
+
+twoDoubleBeds: {type: Number, required: true},
+oneKingBed:{type: Number, required: true},
+
+
+
+
+
+});
--- models/guest-model.js
+++ models/guest-model.js
@@ -1,9 +1,19 @@
 import mongoose, { model } from "mongoose";
 
-const schema = new mongoose.Schema()
+const guestSchema = new mongoose.Schema(
+{
+
+name: {type: String, required: true},
+memberId: {type: Number, required: false},
+numberOfGuests:{type: Number, required: true},
+checkIn:{type: String, required:true},
+checkOut:{type: String, required:true}
+
+});
+
+export const guest = new model("guest",guestSchema);
 
 
 
 
 
-export default model
