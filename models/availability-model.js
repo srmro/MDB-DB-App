43cc1066c2a7adfa990926ba6e4913466d727c4a
srmro <833stephanie@gmail.com>
2025-02-04 20:00:14 -0700

    creating some validation.

--- models/availability-model.js
+++ models/availability-model.js
@@ -1,11 +1,34 @@
 import mongoose, { Schema } from "mongoose";
 
-const availabilitySchema = new mongoose.Schema ({
-
-twoDoubleBeds: {type: Number, required: true},
-oneKingBed:{type: Number, required: true},
+const availabilitySchema = new mongoose.Schema({
 
 
+    totalRooms: {
+        type: Number,
+        required: true,
+        max: 24
+    },
+
+    available: {
+        type: Number,
+        required: true,
+        max: [24,"We're booked for the night!"],
+        min: [18, "Available Rooms to Book!"]
+    },
+
+    twoDoubleBeds: {
+        type: Number,
+        required: true,
+        max:[12, "No more double beds!"],
+        min:[8, "Available Double Beds!"]
+    },
+
+    oneKingBed: {
+        type: Number,
+        required: true,
+        max:[12, "No more King Beds!"],
+        min:[10, "Available King beds" ]
+    }
 
 
 
--- models/guest-model.js
+++ models/guest-model.js
@@ -1,17 +1,34 @@
 import mongoose, { model } from "mongoose";
 
 const guestSchema = new mongoose.Schema(
-{
+    {
 
-name: {type: String, required: true},
-memberId: {type: Number, required: false},
-numberOfGuests:{type: Number, required: true},
-checkIn:{type: String, required:true},
-checkOut:{type: String, required:true}
+        name: {
+            type: String,
+            required: true
+        },
+        memberId: {
+            type: Number,
+            required: false,
+            max: 12,
+            min:[5, "The member id should be 12 characters long"]
+        },
+        numberOfGuests: {
+            type: Number,
+            required: true
+        },
+        checkIn: {
+            type: String,
+            required: true
+        },
+        checkOut: {
+            type: String,
+            required: true
+        }
 
-});
+    });
 
-export const guest = new model("guest",guestSchema);
+export const guest = new model("guest", guestSchema);
 
 
 
